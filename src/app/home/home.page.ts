2aca853df9db59c0277e80b6a90d8def02eb2ff3
andersonar12 <aromerocangri.14@gmail.com>
2021-10-22 15:17:53 -0400

    envio de correo y whatsapp integrados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,28 +1,105 @@
 import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { IonContent } from '@ionic/angular';
 declare var $:any;
+import { LoadingController } from '@ionic/angular';
+import { ClubifyService } from '../services/clubify.service';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
   @ViewChild(IonContent, { static: false }) content: IonContent;
+  formGroup: FormGroup;
 
-  constructor() {
+  constructor(private _formBuilder: FormBuilder,public loadingController: LoadingController,public clubifyService:ClubifyService,public alertController: AlertController) {
   }
 
+  public loading:any;
   ngOnInit(){
 
+    this.formGroup = new FormGroup({
+      first_name:new FormControl( '', Validators.required),
+      last_name:new FormControl( '', Validators.required),
+      email:new FormControl('', [Validators.required,Validators.email]),
+      phone:new FormControl('', Validators.required),
+      message: new FormControl('', Validators.required),
+      prefix:new FormControl( '', Validators.required),
+      /* password: new FormControl('', [Validators.required,Validators.minLength(8)]), */
+    });
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'Cargando...',
+      mode:'md',
+      /* duration:4000 */
+    });
+
+    this.loading.present();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Listo',
+      message: 'Gracias por contactarnos!',
+      buttons: [
+         {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.loading.dismiss()
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  setPrefix(event){
+
+    /* console.log(event.target.value); */
+    const number = event.target.value.replace('+','')
+    event.target.value = `+${number}`
+  }
+
+  async sendMessage(){
+    console.log(this.formGroup.value);
+    await this.presentLoading()
+
+    try {
+      
+      /* await this.clubifyService.sendMail(this.formGroup.value).toPromise().then((resp)=>{
+        console.log(resp)
+      })
+   */
+      await this.clubifyService.sendWhatsapp(this.formGroup.value).toPromise().then((resp)=>{
+        console.log(resp);
+        this.presentAlert()
+        this.loading.dismiss()
+      })
+      
+      this.presentAlert()
+
+    } catch (error) {
+      alert('Ha sucedido un error enviando correo o mensaje')
+      this.loading.dismiss()
+    }
+
+    this.formGroup.reset()
   }
 
   logScrolling(event){
     
-    let mybutton = document.getElementById("btn-scroll");
+    let btnScroll = document.getElementById("btn-scroll");
 
-    if (event.detail.scrollTop > 500) mybutton.style.display = "block";
-    else mybutton.style.display = "none";
+    if (event.detail.scrollTop > 500) btnScroll.style.display = "block";
+    else btnScroll.style.display = "none";
     
   }
 
---
+++ src/app/services/clubify.service.ts
@@ -0,0 +1,67 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClubifyService {
+
+  constructor(private http: HttpClient) {}
+
+  sendMail(data){
+    const endpoint = `https://message-backend.gestsol.io/email/send-email`;
+    const headers = new HttpHeaders({'Accept': '*/*'})
+    const body = {
+      to: `${data.email}`,
+      subject: "Bienvenido a Clubify",
+      content: `<div style='display: flex!important;justify-content: center!important; '>
+      <div>
+        <div style='text-align: center!important;box-shadow: 0px 3px 6px #00000029!important;'>
+          <br>
+          <img src='https://clubify-landing.netlify.app/assets/images/clubify.png' style='width: 180px;' alt='' srcset=''>
+          <br>
+          
+    
+        </div>
+        <div style='text-align: center; padding: 30px; width: 480px; box-shadow: 0px 3px 3px #00000029!important; color: black;'>
+      
+          <p style='text-align:start!important'>¡Hola ${data.first_name}! Hemos recibido tu solicitud para contratar Clubify. Tras comprobar tu contacto, enviaremos un correo a esta misma dirección para que escojas el método de afiliación al producto. </p>
+      
+          <br>
+          <div style='background: #FFFFFF !important;box-shadow: 2px 2px 6px #0000001A!important;border: 1px solid #EDEDED!important;border-radius: 5px!important; padding: 0px 20px!important;text-align: center!important'>
+            <p style='color: #363636!important; margin-bottom: 0px!important;font-weight: bold!important;'><strong>${data.first_name} ${data.last_name}</strong>  </p>
+            <p style='color: #363636!important; margin-bottom: 0px!important;font-weight: bold!important;'><strong>${data.phone}</strong> </p>
+            <p style='color: #363636!important; margin-bottom: 0px!important;font-weight: bold!important;'><strong>${data.email}</strong>  </p>
+          </div>
+          <br>
+      
+          <br>
+          <br>
+          <a href='https://clubify-landing.netlify.app/home' target='_blank' style='text-decoration: none;'><span style='color: white;padding: 15px; background-color: #FF5021;box-shadow: 2px 2px 5px #7C290080;
+            border-radius: 10px;font-weight: bold;'>SOLICITAR GO! MOVIE</span> </a>
+      
+        </div>
+      </div>
+    </div>
+  `
+  }
+
+    return this.http.post<any>(endpoint, body/*,  { headers: headers} */)  
+  }
+
+  sendWhatsapp(data){
+    const endpoint = `https://new-send-api-whatsapp.herokuapp.com/whatsapp/sendmessage`;
+    const headers = new HttpHeaders({'Accept': '*/*'})
+    const body = {
+      "code": data.prefix,
+      "phone": data.phone,
+      "message": `¡Hola ${data.first_name}! Hemos recibido tu solicitud para contratar Live GPS. Tras comprobar que eres beneficiario, enviaremos un correo electronico para que escojas el método de entrega de tu producto.`
+  }
+
+    return this.http.post<any>(endpoint, body/* , { headers: headers} */)  
+  }
+
+}
